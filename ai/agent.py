71f10db3cdf31ff21cc8bb15cef0556af81357ba
Vortex-21 <anshumanlaskar1@gmail.com>
2025-04-21 00:47:38 +0530

    minor changes

--- ai/agent.py
+++ ai/agent.py
@@ -14,68 +14,14 @@ def get_agent_instance(role:str, instructions:str):
                 description=role,
                 instructions = instructions,
             )
-# def get_structuring_agent_instance():
-#     return Agent(
-#         tools=[],
-#         model=Gemini(id="gemini-2.0-flash"),
-#         markdown=True,
-#         description="You are an expert at structuring information into JSON format.",
-#         instructions=,
-#         show_tool_calls=True,
-#     )
-
-# def get_video_agent_instance():
-#     return Agent(
-#         tools=[youtube_video_search],
-#         tool_call_limit=100,
-#         model=Gemini(id="gemini-2.0-flash"),
-#         markdown=True,
-#         description="You are an agent designed to search for relevant YouTube videos based on user prompts.",
-#         instructions="""
-#             #PROVIDED INFO:
-#             You will be provided a list of topics.
-
-#             #INFORMATION GATHERING
-#             **ALWAYS USE the youtube_video_search tool** to find the urls of videos which are relatable to the topics mentioned.
-
-#             #RESPONSE GUIDELINES
-#             **Provide a list of titles of vidoes  PAIRED WITH the URLS** in the following format: 
-#             1.Title1: corresponding_video_url
-#             2.Title2: corresponding_video_url
-#             **No need to print anything else.**
-
-#             #REPEAT: THE ABOVE FORMAT SHOULD BE STRICTLY FOLLOWED. MOREOVER VIDEOS SHOULD ALWAYS BE FOUND USING the youtube_video_search tool.
-#             ...
-#         """,
-#         show_tool_calls=False
-
-#     )
 
 if __name__ == "__main__": 
-    personalized_prompt_1 = """
-        - Learning Goal: Machine Learning 
-        - Time Commitment: 7 hrs per week
-        - Preferred Learning Mode: Tutorials and Videos
-        - Current Skill Level: Intermediate
-        - Deadline: 12 weeks
-    """
-
-    # agent = get_agent_instance() 
-    # response = agent.run(personalized_prompt_1)
-    # print("response: ",response.content)
-    # with open("./response.txt","w") as f:
-    #     f.write(response.content)
-
-    # contents="""
+    # personalized_prompt_1 = """
+    #     - Learning Goal: Machine Learning 
+    #     - Time Commitment: 7 hrs per week
+    #     - Preferred Learning Mode: Tutorials and Videos
+    #     - Current Skill Level: Intermediate
+    #     - Deadline: 12 weeks
     # """
-    with open("./response.txt","r") as f:
-        contents = f.read()
-
-    # yt_agent = get_agent_instance()
-    # yt_videos = yt_agent.run(contents)
-
-    # with open("./video_list.txt", "w") as f:
-    #     f.write(yt_videos.content)
 
 
-    # print("video list: ", yt_videos.content)
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI); // No extra options needed
+        console.log(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName:"SaarthiDB"
+        }); // No extra options needed
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1); // Exit the process with failure
     }
 };
-
 module.exports = connectDB;
